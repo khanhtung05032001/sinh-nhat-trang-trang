88c4ee4e1abd7a1b11ee2e22a61b90fdb9fae9bd
khanhtung05032001 <sontung130701@gmail.com>
2021-10-02 17:28:01 +0700

    tung

---
+++ js/yokattaa.js
@@ -0,0 +1,23 @@
+
+    $(".gui").click(function (){
+        $(".popup").show();
+        $(".trang-chu-3").hide();
+    })
+
+    $(".chon4").click(function (){
+        $(".pupp4").show();
+        $(".popup").hide();
+
+    })
+    $("ipnhap").keypress(function (){
+        $(this).text("tung");
+    })
+
+
+
+
+
+
+
+
+
